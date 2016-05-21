2f80f94a63bb15618ced48f87890bc534076b111
Anthony Winters <a.r.winters@nforce-it.nl>
2016-05-21 21:58:28 +0200

    Added Mongo Client and Db Schema - fixes

--- routes/app.js
+++ routes/app.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
-router.get('/:userTitle?', function(req, res, next) {
-    res.render('node', {title: req.params.userTitle ? req.params.userTitle : '' });
+router.get('/', function(req, res, next) {
+    var email = '';
+    User.findOne({}, function(err, doc) {
+        if (err) {
+            return res.send('Error getting data from NodeJS');
+        }
+        if (doc) {
+            email = doc.email;
+        }
+        res.render('node', {email: email});
+    });
 });
 
 router.post('/', function(req, res, next) {
-    var title = req.body.title;
-    res.redirect('/' + title);
+    var email = req.body.email;
+    var user =  new User({
+        firstName: 'Anthony',
+        lastName: 'Winters',
+        password: 'super-secret',
+        email: email
+    });
+    user.save();
+    res.redirect('/');
 });
 
 module.exports = router;
