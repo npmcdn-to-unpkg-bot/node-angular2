6b4b895dfbdf4bb838340a11afb2a64dfea4ffcc
Anthony Winters <a.r.winters@nforce-it.nl>
2016-05-23 02:32:59 +0200

    Styling the frontend

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -2,14 +2,46 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `  
-         <article class="panel panel-default">
-             <div class="panel-body">
-                {{ message }}
-             </div>
-        </article>
-    `
+      <div class="row">
+            <section class="col-md-8 col-md-offset-2">
+                     <article class="panel panel-default">
+                         <div class="panel-body">
+                            {{ message.content }}
+                        </div>
+                        <footer class="panel-footer">
+                              <div class="author">
+                                   {{ message.author }}
+                              </div>
+                              <div class="config">
+                                 <a href="#">Edit</a>
+                                 <a href="#">Delete</a>
+                             </div>
+                        </footer>
+                     </article>    
+            </section>
+    </div>
+
+    `,
+    styles: [
+        `
+            .author {
+                display: inline-block;
+                font-style: italic;
+                font-size: 12px;
+                width: 80%;
+            }
+            .config {
+                display: inline-block;
+                text-align: right;
+                font-size: 12px;
+                width: 19%;
+            }
+        `]
 })
 export class AppComponent {
-    message = 'A message';
+    message = {
+        content: 'A message',
+        author: 'Anthony'
+    };
 
 }
